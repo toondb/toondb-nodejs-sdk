418d2673419ef4ac70382ef9b35d197b67567d07
sushanth <sushanth53@gmail.com>
2026-01-26 23:45:25 -0800

    chore: Update version references to 0.4.6

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /**
- * SochDB Node.js SDK v0.4.5
+ * SochDB Node.js SDK v0.4.6
  * 
  * Dual-mode architecture: Embedded (FFI) + Server (gRPC/IPC)
  * 
@@ -51,7 +51,7 @@
  */
 
 // Version
-export const VERSION = '0.4.5';
+export const VERSION = '0.4.6';
 
 // Embedded mode (FFI) - NEW
 export { EmbeddedDatabase, EmbeddedDatabaseConfig } from './embedded';
